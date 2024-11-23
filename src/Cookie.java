8dd64b00e7e3a5babebf72f0ed014f2e046f295c
Shatilov Alexey <shatilov.1996@mail.ru>
2024-11-23 23:12:17 +0300

    First commit

---
+++ src/Cookie.java
@@ -0,0 +1,7 @@
+public class Cookie  {
+    Cookie cookie = new Cookie();
+
+    public void bake() {
+        System.out.println("Pechen'ka");
+    }
+}
---
+++ src/Knight.java
@@ -0,0 +1,19 @@
+public class Knight {
+    private String name = "Sir Thanks-A-Lot";
+    private String weapon = "Long Sword";
+    private Boolean isGoingToSavePrincess = true;
+
+    public static void main(String[] args) {
+
+    }
+
+    public void goAndSaveThePrincess() {
+        System.out.println("Да иду уже...");
+    }
+}
+public static void main(String[] args) {
+    // Явили миру рыцаря
+    Knight knight = new Knight();
+    // Отправляем его за принцессой
+    knight.goAndSaveThePrincess();
+}
